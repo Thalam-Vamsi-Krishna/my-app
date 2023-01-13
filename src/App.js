f9d2fc9e512ba6ea6d49cc6c3904a24e01af620a
Thalam-Vamsi-Krishna <vamsikrishna.thalam@gmail.com>
2023-01-13 11:59:11 +0530

    successfully added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenseDetails = [
+    { item: "Food", location: "Tirupati", price: "$ 50" },
+    { item: "Petrol", location: "Tirupati", price: "$ 100" },
+    { item: "Movie", location: "Chennai", price: "$ 200" },
+    { item: "Shopping", location: "Chennai", price: "$ 300" },
+  ];
   return (
     <div>
       <h2>ExpenseItems</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenseDetails.map((expense) => (
+        <ExpenseItem
+          item={expense.item}
+          location={expense.location}
+          price={expense.price}
+        ></ExpenseItem>
+      ))}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,12 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
-    <div>
-      <div className="expense-item">
+    <div className="expense-item">
+      <div className="expense-item__description">
+        <h2>{props.item}</h2>
         <div className="expense-item__description">
-          <h2>Food</h2>
-          <div className="expense-item__price">$ 10</div>
-        </div>
-      </div>
-      <div className="expense-item">
-        <div className="expense-item__description">
-          <h2>Petrol</h2>
-          <div className="expense-item__price">$ 100</div>
-        </div>
-      </div>
-      <div className="expense-item">
-        <div className="expense-item__description">
-          <h2>Movies</h2>
-          <div className="expense-item__price">$ 200</div>
+          <h2>{props.location}</h2>
+          <div className="expense-item__price">{props.price}</div>
         </div>
       </div>
     </div>
